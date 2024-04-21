3d90781f2e9c20ab696b453a7ea50f9681784551
Sri Charan <sricharankota2000@gmail.com>
2024-04-21 11:57:35 +0530

    third commit

--- input.js
+++ input.js
@@ -1,6 +1,7 @@
 function sayHello(){
+    let name=document.querySelector('input').value;
 
-    let name=document.getElementById("name").value;
+    //let name=document.getElementById("name").value;
     let greeting="Hello "+name+" !";
     
     document.getElementById("output").textContent=greeting;
